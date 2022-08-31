edf14f66f25220590290a43f70be1d79a71e738b
tonybutzer <tonybutzer@gmail.com>
2022-08-31 23:20:41 +0000

    auto commit from tony - sept2022

---
+++ et_clone_home.sh
@@ -0,0 +1,12 @@
+# /bin/bash
+
+
+echo $HOME
+
+echo 'what is the instance name?'
+read instance
+
+echo SAVING $instance to s3
+
+
+aws s3 sync $HOME s3://ws-in/tony/$instance/backup/ --exclude $HOME'/miniconda3/*'
